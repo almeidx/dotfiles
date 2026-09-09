fe894854735cb61e75f555f2d7aa5de031214327
Almeida <github@almeidx.dev>
2026-09-09 10:16:48 +0100

    Comment out zshrc replacement and plugin installations
    Comment out commands for moving zshrc and cloning plugins.

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Replace the default zshrc with the one on this repo
-mv ~/.zshrc ~/.zshrc.old
-mv .zshrc.new ~/.zshrc
+# mv ~/.zshrc ~/.zshrc.old
+# mv .zshrc.new ~/.zshrc
 
 # Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Install zsh-completions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+# git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 # Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Install JetBrains Mono font
 # wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
