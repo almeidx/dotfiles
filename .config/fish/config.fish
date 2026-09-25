19c46167913bb88fbfd378b521fe16bd16f3ab00
Almeida <github@almeidx.dev>
2026-09-25 11:10:16 +0100

    Refactor fish config with new environment variables and abbreviations
    Updated fish configuration by adding PNPM_HOME and modifying the fish_prompt function. Replaced aliases with abbreviations for git and pnpm commands.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,72 +1,89 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-function fish_greeting
-end
+function fish_greeting; end
 
-# General
-set -g fish_greeting
-
-# keybindings
-bind \e\[1\;5C forward-word       # Ctrl+Right
-bind \e\[1\;5D backward-word      # Ctrl+Left
-bind \e\[H beginning-of-line      # Home
-bind \e\[F end-of-line            # End
-bind \e\[3\~ delete-char          # Delete
-bind \cH backward-kill-word       # Ctrl+Backspace
-bind \e\[3\;5\~ kill-word         # Ctrl+Delete
-
-# prompt
-function fish_prompt
-    set_color cyan
-    printf '%s' (prompt_pwd)
-
-    set_color yellow
-    printf '%s' (fish_vcs_prompt)
-
-    printf '\n'
-    set_color green
-    printf 'λ '
-    set_color normal
-end
-
-# homebrew
 if test -x /home/linuxbrew/.linuxbrew/bin/brew
     /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 end
 
-# fnm
-if type -q fnm
-    fnm env --use-on-cd --shell fish --resolve-engines | source
+set -gx PNPM_HOME $HOME/.local/share/pnpm
+
+fish_add_path -g \
+    $HOME/.local/bin \
+    $PNPM_HOME/bin
+
+function fish_prompt
+    set -l last_status $status
+
+    set_color cyan
+    printf '%s' (prompt_pwd)
+    set_color yellow
+    printf '%s' (fish_vcs_prompt)
+    printf '\n'
+
+    if test $last_status -eq 0
+        set_color green
+    else
+        set_color red
+    end
+    printf 'λ '
+    set_color normal
 end
 
-# git shortcuts
-alias g='git'
-alias ga='git add'
-alias gb='git branch'
-alias gc='git commit'
-alias gca='git commit --amend'
-alias gcan='git commit --amend --no-edit'
-alias gcb='git switch -c'
-alias gcm='git commit -m'
-alias gco='git checkout'
-alias gcp='git cherry-pick'
-alias gd='git diff'
-alias gfa='git fetch --all --prune'
-alias gp='git push'
-alias gpf='git push --force-with-lease'
-alias gpr='git pull --rebase'
-alias gr='git rebase'
-alias grc='git rebase --continue'
-alias gst='git status -sb'
-alias gsu='git stash -u'
-alias gsp='git stash pop'
-alias gw='git switch'
+function claudex --wraps claude --description 'claude via local gateway'
+    set -l model gpt-6-astra
 
-# pnpm shortcuts
-alias pb='pnpm run build'
-alias pd='pnpm run dev'
-alias pt='pnpm test'
+    env -u ANTHROPIC_API_KEY -u CLAUDE_CODE_OAUTH_TOKEN \
+        ANTHROPIC_BASE_URL=http://127.0.0.1:8317 \
+        ANTHROPIC_AUTH_TOKEN=$CLAUDEX_AUTH_TOKEN \
+        ANTHROPIC_DEFAULT_OPUS_MODEL=$model \
+        ANTHROPIC_DEFAULT_SONNET_MODEL=$model \
+        ANTHROPIC_DEFAULT_HAIKU_MODEL=$model \
+        CLAUDE_CODE_SUBAGENT_MODEL=$model \
+        CLAUDE_CODE_ENABLE_GATEWAY_MODEL_DISCOVERY=1 \
+        CLAUDE_CODE_MAX_CONTEXT_TOKENS=1050000 \
+        CLAUDE_CODE_AUTO_COMPACT_WINDOW=900000 \
+        ENABLE_TOOL_SEARCH=false \
+        claude --model $model $argv
+end
 
-# Other shortcuts
-alias cls='clear'
-alias nr='node --run'
+if status is-interactive
+    # fnm
+    if type -q fnm
+        fnm env --use-on-cd --shell fish --resolve-engines | source
+    end
+
+    bind ctrl-backspace backward-kill-word
+    bind ctrl-delete kill-word
+
+    # git
+    abbr -a g git
+    abbr -a ga git add
+    abbr -a gb git branch
+    abbr -a gc git commit
+    abbr -a gca git commit --amend
+    abbr -a gcan git commit --amend --no-edit
+    abbr -a gcb git switch -c
+    abbr -a gcm git commit -m
+    abbr -a gco git checkout
+    abbr -a gcp git cherry-pick
+    abbr -a gd git diff
+    abbr -a gfa git fetch --all --prune
+    abbr -a gp git push
+    abbr -a gpf git push --force-with-lease
+    abbr -a gpr git pull --rebase
+    abbr -a gr git rebase
+    abbr -a grc git rebase --continue
+    abbr -a gst git status -sb
+    abbr -a gsu git stash -u
+    abbr -a gsp git stash pop
+    abbr -a gw git switch
+
+    # pnpm / node
+    abbr -a pb pnpm run build
+    abbr -a pd pnpm run dev
+    abbr -a pt pnpm test
+    abbr -a nr node --run
+
+    abbr -a cls clear
+end
